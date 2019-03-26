0970af4e380d1b4747781ad0a41038e2c683310e
adri52 <48935267+adri52@users.noreply.github.com>
2019-03-26 00:14:04 -0600

    aditional coments

--- Lab9Files/StringDLinkedList.h
+++ Lab9Files/StringDLinkedList.h
@@ -1,6 +1,9 @@
 #ifndef STRINGDLL_H
 #define STRINGDLL_H
 #include<iostream>
+#include <string>
+
+using namespace std; 
 
 class doubleStringList
 {
@@ -9,18 +12,37 @@ private:
 	struct ListNode
 	{
 		string data;
-		struct ListNode *previous;  //pointer to the previous node 
+		struct ListNode *prev;  //pointer to the previous node 
 		struct ListNode *next;  //pointer to the next node 
 	};
+
+	string * head; 
+	string * tail;
+
 public:
 
-    //The class should have a default constructor
+    //default constructor
+	doubleStringList(); 
 	//a destructor: to delete any remaining nodes, member functions to access, delete and append a value at either end of the list 
+	~doubleStringList();
 	// a member function to remove all instances of a specific value
+	void remove();  //deletes all the values in the list? 
+	//*************************************************************************************************************
 	// a member function to display the current contents of the entire list either 
 	//forwards or 
 	//backwards (these two function should accept a ostream reference to print to which will make them easy to test).
-	//(i.e. you should support both ends; please name these front, back, push_front, push_back, pop_front, pop_back)
+	//*****************************************************************************************************************
+	//front (return the value of the first thing in the list?)
+	//back (return the value of the first thing in the list?)
+
+	//push_front: Adds a string in front of the begining of the list 
+	void push_front(const string & value);
+	//push_back: Adds a string in the back of the end of the list 
+	void push_back(const string & value);
+	//pop_front: Removes a string in front of the begining of the list 
+	void pop_front();
+	//pop_back: Removes a string in the back of the end of the list 
+	void pop_back(); 
 };
 
 
